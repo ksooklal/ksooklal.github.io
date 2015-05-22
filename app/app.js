cc5beadfb4d4ad4fd7b86403ffe6b24a04baa47d
Kristian Sooklal <kristiansooklal@gmail.com>
2015-05-22 16:11:55 -0400

    index.html

--- app/app.js
+++ app/app.js
@@ -5,4 +5,22 @@ myAppModule.controller("IndexController", function ($scope){
 		var targetID = elementID.substring(0, elementID.length - 3);
 		$("#" + targetID).focus();
 	};
+	
+	$scope.projects = [
+		{
+			project: "Stack",
+			projectDescription: "Constant Time implementation of basic Stack functionality, applicable for any Generic object that implements Comparable interface",
+			projectTechnologies: "Java"
+		},
+		{
+			project: "LinkedList",
+			projectDescription: "Implementation of classic LinkedList data structure in Java, with Generic compatibility", 
+			projectTechnologies: "Java"
+		},
+		{
+			project: "Change Picture Example",
+			projectDescription: "Adding custom action to picture object using JQuery",
+			projectTechnologies: "HTML, JavaScript, JQuery"
+		}
+	];
 });
