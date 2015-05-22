52de840ee77c5f4c22b3383342ca08e8c85a481e
Kristian Sooklal <kristiansooklal@gmail.com>
2015-05-22 17:03:48 -0400

    index.html

--- app/app.js
+++ app/app.js
@@ -1,26 +1,31 @@
 var myAppModule = angular.module("myApp", []);
-myAppModule.controller("IndexController", function ($scope){
-	$scope.focusOn = function(event){
-		var elementID = event.target.id;
-		var targetID = elementID.substring(0, elementID.length - 3);
-		$("#" + targetID).focus();
-	};
-	
-	$scope.projects = [
-		{
-			project: "Stack",
-			projectDescription: "Constant Time implementation of basic Stack functionality, applicable for any Generic object that implements Comparable interface",
-			projectTechnologies: "Java"
-		},
-		{
-			project: "LinkedList",
-			projectDescription: "Implementation of classic LinkedList data structure in Java, with Generic compatibility", 
-			projectTechnologies: "Java"
-		},
-		{
-			project: "Change Picture Example",
-			projectDescription: "Adding custom action to picture object using JQuery",
-			projectTechnologies: "HTML, JavaScript, JQuery"
-		}
-	];
-});
+myAppModule.controller("IndexController", [
+	function (){
+		this.focusOn = function(event){
+			var elementID = event.target.id;
+			var targetID = elementID.substring(0, elementID.length - 3);
+			$("#" + targetID).focus();
+		};
+		
+		this.projects = [
+			{
+				projectName: "Stack",
+				projectLink: "https://github.com/ksooklal/Stack",
+				projectDescription: "Constant Time implementation of basic Stack functionality, applicable for any Generic object that implements Comparable interface",
+				projectTechnologies: "Java"
+			},
+			{
+				projectName: "LinkedList",
+				projectLink: "",
+				projectDescription: "Implementation of classic LinkedList data structure in Java, with Generic compatibility", 
+				projectTechnologies: "Java"
+			},
+			{
+				projectName: "Change Picture Example",
+				projectLink: "https://github.com/ksooklal/Change-Picture-Example",
+				projectDescription: "Adding custom action to picture object using JQuery",
+				projectTechnologies: "HTML, JavaScript, JQuery"
+			}
+		];
+	}
+]);
