a31878bab937b298fffe4d2b6cd01a47549dbce8
Kristian Sooklal <kristiansooklal@gmail.com>
2015-05-28 17:41:19 -0400

    added some responsive aspects to the page
    index.html changes
    app.js changes

--- app/app.js
+++ app/app.js
@@ -4,29 +4,33 @@ myAppModule.controller("IndexController", [
 		this.focusOn = function(event){
 			var elementID = event.target.id;
 			var targetID = elementID.substring(0, elementID.length - 3);
-			$("#" + targetID).focus();
+			$(window).scrollTo("#" + targetID);
 		};
 		
 		this.projects = [
 			{
 				projectName: "Stack",
+				projectID: "Stack",
 				projectLink: "https://github.com/ksooklal/Stack",
 				projectDescription: "Constant Time implementation of basic Stack functionality, applicable for any Generic object that implements Comparable interface",
 				projectTechnologies: "Java"
 			},
 			{
 				projectName: "LinkedList",
+				projectID: "LinkedList",
 				projectLink: "https://github.com/ksooklal/LinkedList",
 				projectDescription: "Implementation of classic LinkedList data structure in Java, with Generic compatibility", 
 				projectTechnologies: "Java"
 			},
 			{
 				projectName: "ArrayList",
+				projectID: "ArrayList",
 				projectLink: "https://github.com/ksooklal/ArrayList",
 				projectDescription: "Implementation of classic ArrayList data structure in Java, with Generic compatibility", 
 				projectTechnologies: "Java"
 			},
 			{
+				projectID: "ChangePictureExample",
 				projectName: "Change Picture Example",
 				projectLink: "https://github.com/ksooklal/Change-Picture-Example",
 				projectDescription: "Adding custom action to picture object using JQuery",
