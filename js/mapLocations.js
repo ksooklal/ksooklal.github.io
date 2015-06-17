0a0267bbad288065451b93f79e7be8ad1fc2e1e0
Kristian Sooklal <kristiansooklal@gmail.com>
2015-06-17 13:47:34 -0400

    added map locations

--- js/mapLocations.js
+++ js/mapLocations.js
@@ -1,12 +1,20 @@
 var locations = [];
 locations.push(new Location("COD", "Work", 38.9007471, -77.00649629999998, "820 First Street NE Washington DC 20002"));
-locations.push(new Location("Kristian", "Home", 38.9931229, -77.0234289, "820 First Street NE Washington DC 20002"));
+locations.push(new Location("Kristian", "Home", 38.9931229, -77.0234289, "815 Thayer Avenue Apt 520 Silver Spring MD 20910"));
+locations.push(new Location("Baxter", "Home", 38.90051, -77.01837, "450 Massachusetts Avenue Northwest Washington DC 20001"));
+
+
+
+
 locations.push(new Location("Pierce St NE & First St NE", "Parking", 38.9046994, -77.0062291, "88 Pierce Street NE Washington DC 20002"));
 locations.push(new Location("Pierce St NE", "Parking", 38.904659, -77.008428, "25 Pierce Street NE Washington DC 20002"));
 locations.push(new Location("Pierce St NE & North Capitol St", "Parking", 38.904737, -77.008810,  "8 Pierce Street NE Washington DC 20002"));
 locations.push(new Location("Pierce St NE & North Capitol St", "Parking", 38.904671, -77.008881,  "5 Pierce Street NE Washington DC 20002"));
-locations.push(new Location("North Capitol St NE", "Parking", 38.904219, -77.008951,  "1109 North Capitol St NE Washington DC 20002"));
-	
+locations.push(new Location("North Capitol St NE", "Parking", 38.904219, -77.008951, "1109 North Capitol St NE Washington DC 20002"));
+locations.push(new Location("North Capitol St NW", "Parking", 38.904064, -77.009171, "1072 North Capitol St NW Washington DC 20002"));
+locations.push(new Location("L Street Alley", "Parking", 38.904001, -77.011421, "1100 McKenna Walk NW Washington DC 20001"));
+locations.push(new Location("L Street", "Parking", 38.903840, -77.011621, "87 L Street NW Washington DC 20001"));
+
 function getLocations(){
 	return locations;
 }
