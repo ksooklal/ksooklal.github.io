986e03566bceaf4943365301d7e3180fb44c1864
Kristian Sooklal <kristiansooklal@gmail.com>
2015-06-17 13:11:50 -0400

    added location

--- js/mapLocations.js
+++ js/mapLocations.js
@@ -5,6 +5,7 @@ locations.push(new Location("Pierce St NE & First St NE", "Parking", 38.9046994,
 locations.push(new Location("Pierce St NE", "Parking", 38.904659, -77.008428, "25 Pierce Street NE Washington DC 20002"));
 locations.push(new Location("Pierce St NE & North Capitol St", "Parking", 38.904737, -77.008810,  "8 Pierce Street NE Washington DC 20002"));
 locations.push(new Location("Pierce St NE & North Capitol St", "Parking", 38.904671, -77.008881,  "5 Pierce Street NE Washington DC 20002"));
+locations.push(new Location("North Capitol St NE", "Parking", 38.904219, -77.008951,  "1109 North Capitol St NE Washington DC 20002"));
 	
 function getLocations(){
 	return locations;
