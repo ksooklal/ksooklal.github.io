093e5373fc184d3eaabd473f3417aad0a499044d
Kristian Sooklal <kristiansooklal@gmail.com>
2015-06-17 11:45:36 -0400

    added utility files

---
+++ js/mapUtilities.js
@@ -0,0 +1,52 @@
+function ParkingDescription(){
+	this.zoneRestriction = 0;
+	this.streetCleaning = false;
+	this.cost = 0;
+	this.garage = false;
+}
+
+function ParkingDescription(zoneRestriction, streetCleaning, cost, side){
+	this.cost = cost;
+	this.streetCleaning = streetCleaning;
+	this.garage = false;
+	this.zoneRestriction = zoneRestriction;
+	this.side = side;
+}
+
+function ParkingDescription(zoneRestriction, streetCleaning, cost){
+	this.cost = cost;
+	this.streetCleaning = streetCleaning;
+	this.garage = false;
+	this.zoneRestriction = zoneRestriction;
+	this.side='Right';
+}
+
+function Location(locationName, locationType, latitude, longitude, locationAddress, zIndex, parkingDescription) {
+	this.locationName = locationName;
+	this.locationType = locationType;
+	this.latitude = latitude;
+	this.longitude = longitude;
+	this.locationAddress = locationAddress;
+	this.zIndex = zIndex;
+	this.parkingDescription = parkingDescription;
+}
+
+function Location(locationName, locationType, latitude, longitude, locationAddress, parkingDescription) {
+	this.locationName = locationName;
+	this.locationType = locationType;
+	this.latitude = latitude;
+	this.longitude = longitude;
+	this.locationAddress = locationAddress;
+	this.zIndex = 1;
+	this.parkingDescription = parkingDescription;
+}
+
+function Location(locationName, locationType, latitude, longitude, locationAddress){
+	this.parkingDescription = null;
+	this.zIndex = 1;
+	this.latitude = latitude;
+	this.locationType = locationType;
+	this.locationAddress = locationAddress;
+	this.locationName = locationName;
+	this.longitude = longitude;
+}
