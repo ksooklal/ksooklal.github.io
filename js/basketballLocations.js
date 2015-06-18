524567a90c4be471749af25953874d04828a61f6
Kristian Sooklal <kristiansooklal@gmail.com>
2015-06-18 10:26:55 -0400

    added more location files

---
+++ js/basketballLocations.js
@@ -0,0 +1,6 @@
+var basketballLocations = [];
+basketballLocations.push(new Location("Virginia Highlands Park", 38.858216, -77.060415, "Virginia Highlands Park, Arlington, VA 22202"));
+
+function getBasketballLocations(){
+	return basketballLocations;
+}
