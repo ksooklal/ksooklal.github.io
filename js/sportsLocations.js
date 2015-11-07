d1d4a8af0d2076b72a4f9ef7e04b3c4d537d1ed4
Kristian Sooklal <kristiansooklal@gmail.com>
2015-11-06 23:51:33 -0500

    added more locations

--- js/sportsLocations.js
+++ js/sportsLocations.js
@@ -15,10 +15,19 @@ locations.push(new SportsLocation({
 }));
 
 locations.push(new SportsLocation({
-	league: "",
-	locationName: "", sportsCity: "", locationCity: "", latitude: "", longitude: "",
-	gameName: "", date: "", gameDescription: "",
-	ticketUrl: ""
+	league: "NBA",
+	locationName: "Barclays Center", sportsCity: "Brooklyn", locationCity: "Brooklyn", 
+	latitude: "40.6829896", longitude: "-73.9752615",
+	gameName: "Brooklyn Nets vs Washington Wizards", date: "04/10/2015", gameDescription: "Regular Season Game",
+	ticketUrl: "../../Sports Travels/NBA/Brooklyn Nets vs Washington Wizards 2015-04-10.pdf"
+}));
+
+locations.push(new SportsLocation({
+	league: "NBA",
+	locationName: "Philadelphia 76ers", sportsCity: "Philadelphia", locationCity: "Phildelphia", 
+	latitude: "39.9134618", longitude: "-75.1716640",
+	gameName: "Philadelphia 76ers vs Washington Wizards", date: "04/08/2015", gameDescription: "Regular Season Game",
+	ticketUrl: "../../Sports Travels/NBA/Philadelphia 76ers vs Washington Wizards 2015-04-08.pdf"
 }));
 
 function getSportsLocations(){
