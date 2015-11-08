0718599ac107864081154fcdca5772936253e38d
Kristian Sooklal <kristiansooklal@gmail.com>
2015-11-07 21:37:42 -0500

    added some changes

--- js/sportsLocations.js
+++ js/sportsLocations.js
@@ -24,11 +24,27 @@ locations.push(new SportsLocation({
 
 locations.push(new SportsLocation({
 	league: "NBA",
-	locationName: "Philadelphia 76ers", sportsCity: "Philadelphia", locationCity: "Phildelphia", 
+	locationName: "Wells Fargo Center", sportsCity: "Philadelphia", locationCity: "Phildelphia", 
 	latitude: "39.9134618", longitude: "-75.1716640",
 	gameName: "Philadelphia 76ers vs Washington Wizards", date: "04/08/2015", gameDescription: "Regular Season Game",
 	ticketUrl: "../../Sports Travels/NBA/Philadelphia 76ers vs Washington Wizards 2015-04-08.pdf"
 }));
+
+locations.push(new SportsLocation({
+	league: "MLB",
+	locationName: "Camden Yards", sportsCity: "Baltimore", locationCity: "Baltimore", 
+	latitude: "39.284", longitude: "-76.622",
+	gameName: "Baltimore Orioles vs Toronto Blue Jays", date: "09/30/2015", gameDescription: "Regular Season Game",
+	ticketUrl: "../../Sports Travels/MLB/Baltimore Orioles vs Toronto Blue Jays 2015-09-30.pdf"
+}));
+
+/* locations.push(new SportsLocation({
+	league: "",
+	locationName: "", sportsCity: "", locationCity: "", 
+	latitude: "", longitude: "",
+	gameName: "", date: "", gameDescription: "",
+	ticketUrl: ""
+})); */
 
 function getSportsLocations(){
 	return locations;
