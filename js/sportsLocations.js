5b5968c94be6c55c0bf2d9dcb2f7b42312616dfe
Kristian Sooklal <kristiansooklal@gmail.com>
2016-06-23 14:50:46 -0400

    updated sports locations

--- js/sportsLocations.js
+++ js/sportsLocations.js
@@ -47,7 +47,7 @@ locations.push(new SportsLocation({
 locations.push(new SportsLocation({
 	league: "NBA", type: "NBA",
 	locationName: "Wells Fargo Center", sportsCity: "Philadelphia", locationCity: "Phildelphia", 
-	latitude: "39.90121", longitude: "-75.171985",
+	latitude: "39.901204", longitude: "-75.17198",
 	gameName: "Philadelphia 76ers vs Washington Wizards", date: "04/08/2015", gameDescription: "Regular Season Game",
 	ticketUrl: "../../Sports Travels/NBA/Philadelphia 76ers vs Washington Wizards 2015-04-08.pdf"
 }));
