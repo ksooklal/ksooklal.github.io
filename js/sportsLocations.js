51ddb776b0288bf44c5b3c56a822fd9c1d236f50
Kristian Sooklal <ksooklal@terpmail.umd.edu>
2017-12-11 00:43:10 -0500

    updated maryland game url

--- js/sportsLocations.js
+++ js/sportsLocations.js
@@ -547,7 +547,7 @@ locations.push(new SportsLocation({
 	locationName: "Xfinity Center", sportsCity: "College Park", locationCity: "College Park, MD", 
 	latitude: "38.9954727", longitude: "-76.9415108",
 	gameName: "Maryland Terrapins vs. Michigan State Spartans", date: "01/17/2015", gameDescription: "Regular Season Game",
-	ticketUrl: "../../Sports Travels/NCAA Basketball/Maryland Terrapins vs Michigan State 2016-01-17.pdf"
+	ticketUrl: "../../Sports Travels/NCAA Basketball/Maryland Terrapins vs Michigan State 2015-01-17.pdf"
 }));
 
 //NCAAFB, Visited
